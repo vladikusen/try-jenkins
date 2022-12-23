3cd2b696df5c16f52e396bf7a9739c7734653ad4
Vladyslav Shkilniuk <shkilniuk.v@ajax.systems>
2022-12-23 14:09:21 +0200

    added qt part

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/Documents/try_jenkins
+# Build directory: /home/user/Documents/try_jenkins/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
